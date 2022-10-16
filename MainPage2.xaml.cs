746b29ed29502466d95b3cd60dd6566956d89a09
Thilo Viereck <codedev@profix898.de>
2022-10-16 18:15:43 +0200

    Initial commit

---
+++ MainPage2.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage2 : ContentPage
+{
+	int count = 0;
+
+	public MainPage2()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void OnSwitchMainPage(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new NavigationPage(new MainPage());
+    }
+}
+
